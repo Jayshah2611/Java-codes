cd28955cc9583ea06c9d83c4d63f35180b0435ec
Jay Shah <jayshah02611@gmail.com>
2026-01-13 15:50:32 +0530

    Create P1_MeterToFeet.java

---
+++ Java Practical's /P1_MeterToFeet.java
@@ -0,0 +1,20 @@
+/**Develop a Java program that prompts the user to enter a distance in meters. 
+Your program should then convert this distance to feet (1 meter = 3.28084 feet) and 
+display the result formatted to two decimal places.*/
+
+
+import java.util.Scanner;
+class P1{
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("Enter the value in meters : ");
+	double meters = sc.nextDouble();
+
+	double feet = meters * 3.28084;
+
+	System.out.printf("Distance in feet is : %.2f ", feet);
+
+	sc.close();
+	}
+}
