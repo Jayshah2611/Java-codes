880eb77f3f1b878e12a1e3880f643a9b63dc17b3
Jay Shah <jayshah02611@gmail.com>
2026-01-13 14:30:00 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,15 @@
+class Demo{
+
+	public static void main(String[] args) {
+
+		int a = 23;
+		int b = 42;
+
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("a % b = " + (a % b));
+
+	}
+}
